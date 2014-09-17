0894b5509a94ac0d44494d89929767cb673a6e2a
NuclearShop <collaandspar@gmail.com>
2014-09-17 21:31:02 +0400

    'impulse-0.01init'

---
+++ ImpulseApp/ImpulseApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ImpulseApp.Startup))]
+namespace ImpulseApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
