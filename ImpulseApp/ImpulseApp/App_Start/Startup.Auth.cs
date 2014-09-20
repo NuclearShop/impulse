a83c08f29641916a3f046626453e982969f45449
NuclearShop <collaandspar@gmail.com>
2014-09-20 21:26:46 +0400

    test_commit2

--- ImpulseApp/ImpulseApp/App_Start/Startup.Auth.cs
+++ ImpulseApp/ImpulseApp/App_Start/Startup.Auth.cs
@@ -18,7 +18,7 @@ namespace ImpulseApp
             });
             // Use a cookie to temporarily store information about a user logging in with a third party login provider
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
-
+            int a = 0;
 
             // Uncomment the following lines to enable logging in with third party login providers
             //app.UseMicrosoftAccountAuthentication(
